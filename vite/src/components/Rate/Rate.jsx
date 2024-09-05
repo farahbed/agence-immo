adb3af569034c3df39f73e7d80b2283675938425
farahbed <farah.bendriss.la@gmail.com>
2024-09-05 11:21:59 +0200

    responsive header and cards a faire

--- vite/src/components/Rate/Rate.jsx
+++ vite/src/components/Rate/Rate.jsx
@@ -9,8 +9,8 @@ function Rate({ rating }) {
 
     // Boucle pour créer les étoiles
     for (let i = 1; i <= maxStars; i++) {
-      // Utiliser l'image d'étoile remplie si l'indice est inférieur ou égal au rating
-      // Sinon, utiliser l'image d'étoile inactive
+    
+
       const starSrc = i <= rating ? starFilled : starEmpty;
 
       stars.push(<img key={i} src={starSrc} alt="Star" />);
@@ -22,7 +22,7 @@ function Rate({ rating }) {
   return (
     <div className="rate">
       <div className="rating">{renderStars(rating)}</div>
-      {/* Autres éléments de la card */}
+      
     </div>
   );
 }
