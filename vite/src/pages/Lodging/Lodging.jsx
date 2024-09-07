c94b8cd842a2413142e2dab1a74f13114f36ccc9
farahbed <farah.bendriss.la@gmail.com>
2024-09-07 18:59:49 +0200

    modification responsive page lodging

--- vite/src/pages/Lodging/Lodging.jsx
+++ vite/src/pages/Lodging/Lodging.jsx
@@ -28,55 +28,55 @@ function Lodging () {
         tags,} = lodging;
 
     return (
-        <div className="lodging-page">
+ <div className="lodging-page">
             {/* Pass the pictures array to Carouselle */}
             <Carouselle images={pictures} />{" "}
 
             {/* Display other lodging details */}
             
-                <div className="all-infos">
-            <div className="first-case">
+    <div className="all-infos">
+        <div className="first-case">
                 <div className="logInfos">
-                <h2>{title}</h2>
-                <p>{location}</p>
+                    <h2>{title}</h2>
+                    <p>{location}</p>
                 </div>
             <div>
                 <ul>
-                {tags.map((tag, index) => (
+                    {tags.map((tag, index) => (
                     <li key={index}>{tag}</li>
-                ))}
+                 ))}
                 </ul>
             </div>
-            </div>
+        </div>
 
             {/* Deuxième ligne  */}
-            <div className="second-case">
-            <div className="host">
-                <div className="headinfos">
-                {" "}
-                <p>{host.name}</p>
-                <img src={host.picture} alt={host.name} className="host-image" />
-                </div>
-
-                <div className="rate">
-                <Rate rating={parseInt(rating)} />{" "}
+        <div className="second-case">    
+            <div className="rate">
+                 <Rate rating={parseInt(rating)} />{" "}
                 {/* Passer la note en tant que prop */}
+            </div>
+                <div className="host">
+                    <div className="headinfos">
+                        {" "}
+                        <p>{host.name}</p>
+                        <img src={host.picture} alt={host.name} className="host-image" />
+                    </div>
                 </div>
-            </div>
-            </div>
         </div>
+        
 
         <div className="col-container">
             <div className="description">
-            <Description lodgingId={id} />{" "}
-            {/* Passer l'ID du logement en tant que prop */}
+                <Description lodgingId={id} />{" "}
+                {/* Passer l'ID du logement en tant que prop */}
             </div>
             
             <div className="equipements">
-            <Equipement lodgingId={id} data={backData} />
+                <Equipement lodgingId={id} data={backData} />
             </div>
         </div>
-        </div>
+    </div>
+</div>
   );
 }
       
